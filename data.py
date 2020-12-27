8c0e90cae1a3d1ddefdaef88885664813b6414e0
seungpyo <spkbk98@gmail.com>
2020-12-27 01:29:58 +0000

    ADD pickle caches for tickers and quotes

--- data.py
+++ data.py
@@ -12,22 +12,46 @@ from FinanceDataReader._utils import (_convert_letter_to_num, _validate_dates)
 import re
 import pandas as pd
 from datetime import datetime, timedelta
+import pickle as pkl
+import os
 
 def DataReader(symbol, start=None, end=None, exchange=None, data_source=None):
     start, end = _validate_dates(start, end)
+
+    cwd = os.path.split(__file__)[0]
+    if not os.path.isfile(os.path.join(cwd, 'cache', 'ticker_cache.pkl')):
+        ticker_cache = list()
+        with open(os.path.join(cwd, 'cache', 'ticker_cache.pkl'), 'wb') as f:
+            pkl.dump(ticker_cache, f)
+    with open(os.path.join(cwd, 'cache', 'ticker_cache.pkl'), 'rb') as f:
+        ticker_cache = pkl.load(f)
+
+    if symbol in ticker_cache:
+        df = pd.read_pickle((os.path.join(cwd, 'cache', '{0}.pkl'.format(symbol))))
+        return df
+    else:
+        ticker_cache.append(symbol)
+        with open(os.path.join(cwd, 'cache', 'ticker_cache.pkl'), 'wb') as f:
+            pkl.dump(ticker_cache, f)
     
     # FRED Reader
     if data_source and data_source.upper() == 'FRED':
-        return FredReader(symbol, start, end, exchange, data_source).read()
+        ret = FredReader(symbol, start, end, exchange, data_source).read()
+        ret.to_pickle(os.path.join(cwd, 'cache', '{0}.pkl'.format(symbol)))
+        return ret
 
     # KRX and Naver Finance
     if (symbol[:5].isdigit() and exchange==None) or \
        (symbol[:5].isdigit() and exchange and exchange.upper() in ['KRX', '한국거래소']):
-        return NaverDailyReader(symbol, start, end, exchange, data_source).read()
+        ret = NaverDailyReader(symbol, start, end, exchange, data_source).read()
+        ret.to_pickle(os.path.join(cwd, 'cache', '{0}.pkl'.format(symbol)))
+        return ret
 
     # KRX-DELISTINGS
     if (symbol[:5].isdigit() and exchange and exchange.upper() in ['KRX-DELISTING']):
-        return KrxDelistingReader(symbol, start, end, exchange, data_source).read()
+        ret = KrxDelistingReader(symbol, start, end, exchange, data_source).read()
+        ret.to_pickle(os.path.join(cwd, 'cache', '{0}.pkl'.format(symbol)))
+        return ret
 
     # Investing
     reader = InvestingDailyReader
@@ -38,6 +62,7 @@ def DataReader(symbol, start=None, end=None, exchange=None, data_source=None):
         if len(more) == 0:
             break
         df = df.append(more)
+    df.to_pickle(os.path.join(cwd, 'cache', '{0}.pkl'.format(symbol)))
     return df
 
 def StockListing(market):
